4ea9f6c05b11327e37fa1426805c89867e746847
yjn808 <yjnysa@163.com>
2026-04-10 16:46:43 +0800

    PID_control

---
+++ self-balancing scooter/basecode/User/PID.h
@@ -0,0 +1,25 @@
+#ifndef __PID_H
+#define __PID_H
+
+
+typedef struct {
+	float Target;
+	float Actual;
+	float Out;
+	
+	float Kp;
+	float Ki;
+	float Kd;
+	
+	float Error0;
+	float Error1;
+	float ErrorInt;
+	
+	float OutMax;
+	float OutMin;
+} PID_t;
+
+void PID_Update(PID_t *p);
+void PID_Init(PID_t *p);
+
+#endif
--- self-balancing scooter/basecode/User/main.c
+++ self-balancing scooter/basecode/User/main.c
@@ -10,6 +10,9 @@
 #include "Serial.h"
 #include "BlueSerial.h"
 #include <math.h>
+#include "PID.h"
+#include <string.h>
+#include <stdlib.h>
 
 //MPU6050测试
 int16_t AX,AY,AZ,GX,GY,GZ;
@@ -17,9 +20,25 @@ uint8_t TimerErrorFlag;
 uint16_t TimerCount;
 
 
+
 float Angle;
 float AngleGyro;
 float AngleAcc;
+
+uint8_t KeyNum,RunFlag;
+
+int16_t LeftPWM,RightPWM;
+int16_t AvePWM,DifPWM;
+
+PID_t AnglePID={
+	.Kp = 3,
+	.Ki = 0.1,
+	.Kd = 3,
+	
+	.OutMax=100,
+	.OutMin=-100
+};
+
 
 int main(void)
 {
@@ -28,41 +47,149 @@ int main(void)
 	OLED_Init();
 	MPU6050_Init();
 	BlueSerial_Init();
+	LED_Init();
+	Key_Init();
+	Motor_Init();
+	Encoder_Init();
+	Serial_Init();
+	
 	
 	Timer_Init();
 	
 	
 	while(1)
 	{
-	
+		if(RunFlag){LED_ON();}else{LED_OFF();}
 		
-		OLED_Printf(0,0,OLED_8X16,"%+06d",AX);
-		OLED_Printf(0,16,OLED_8X16,"%+06d",AY);
-		OLED_Printf(0,32,OLED_8X16,"%+06d",AZ);
-		OLED_Printf(64,0,OLED_8X16,"%+06d",GX);
-		OLED_Printf(64,16,OLED_8X16,"%+06d",GY);
-		OLED_Printf(64,32,OLED_8X16,"%+06d",GZ);
-		OLED_Printf(0,48,OLED_8X16,"Flag:%1d",TimerErrorFlag);
-		OLED_Printf(64,48,OLED_8X16,"C:%05d",TimerCount);
+		
+		//控制启停
+		KeyNum=Key_GetNum();
+		if(KeyNum == 1)
+		{
+			if(RunFlag==0)
+			{
+				PID_Init(&AnglePID);
+				RunFlag=1;
+			}
+			else{
+				RunFlag = 0;
+			}
+			
+		}
+		
+		OLED_Clear();
+		OLED_Printf(0,0,OLED_6X8,"Angle");
+		OLED_Printf(0,8,OLED_6X8,"P:%05.2f",AnglePID.Kp);
+		OLED_Printf(0,16,OLED_6X8,"I:%05.2f",AnglePID.Ki);
+		OLED_Printf(0,24,OLED_6X8,"D:%05.2f",AnglePID.Kd);
+		OLED_Printf(0,32,OLED_6X8,"T:%+05.2f",AnglePID.Target);
+		OLED_Printf(0,40,OLED_6X8,"A:%+05.1f",AnglePID);
+		OLED_Printf(0,48,OLED_6X8,"O:%+05.0f",AnglePID.Out);
 		OLED_Update();
 		
-		BlueSerial_Printf("[plot,%f,%f,%f]",AngleAcc,AngleGyro,Angle);
-		Delay_ms(20);
+		
+		if (BlueSerial_RxFlag == 1)
+		{
+			char *Tag = strtok(BlueSerial_RxPacket, ",");
+			if (strcmp(Tag, "key") == 0)
+			{
+				char *Name = strtok(NULL, ",");
+				char *Action = strtok(NULL, ",");
+			}
+			else if (strcmp(Tag, "slider") == 0)
+			{
+				char *Name = strtok(NULL, ",");
+				char *Value = strtok(NULL, ",");
+				
+				if(strcmp(Name,"AngleKp") == 0)
+				{
+					AnglePID.Kp=atof(Value);
+				}
+				else if(strcmp(Name,"AngleKi") == 0)
+				{
+					AnglePID.Ki=atof(Value);
+				}
+				else if(strcmp(Name,"AngleKd") == 0)
+				{
+					AnglePID.Kd=atof(Value);
+				}
+			}
+			else if (strcmp(Tag, "joystick") == 0)
+			{
+				int8_t LH = atoi(strtok(NULL, ","));
+				int8_t LV = atoi(strtok(NULL, ","));
+				int8_t RH = atoi(strtok(NULL, ","));
+				int8_t RV = atoi(strtok(NULL, ","));
+				
+				AnglePID.Target=LV/10;
+				DifPWM=RH/2;
+				
+				
+			}
+			
+			BlueSerial_RxFlag = 0;
+		}
+		
+		
+		BlueSerial_Printf("[plot,%f,%f,%f]",AnglePID.Target,Angle);
+		
 	}
 }
 void TIM1_UP_IRQHandler(void)
 {
+	static uint16_t Count0;
+	
+	
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 	{
 		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
-		MPU6050_GetData(&AX,&AY,&AZ,&GX,&GY,&GZ);
 		
-		AngleAcc=-atan2(AX,AZ)/3.14159*180;
+		Key_Tick();
 		
-		AngleGyro=Angle+GY/32768.0*2000*0.001;
+		Count0 ++;
 		
-		float Alpha=0.001;
-		Angle=Alpha * AngleAcc + (1-Alpha) * AngleGyro;
+		if(Count0 >= 10)
+		{
+			Count0=0;
+			MPU6050_GetData(&AX,&AY,&AZ,&GX,&GY,&GZ);
+		
+			AngleAcc=-atan2(AX,AZ)/3.14159*180;
+			
+			AngleGyro=Angle+GY/32768.0*2000*0.01;
+			
+			float Alpha=0.01;
+			Angle=Alpha * AngleAcc + (1-Alpha) * AngleGyro;
+			
+			
+			if(Angle>50 || Angle < -50)
+			{
+				RunFlag = 0;
+			}
+			if(RunFlag)
+			{
+				//PID调控
+				AnglePID.Actual = Angle;
+				PID_Update(&AnglePID);
+				AvePWM=-AnglePID.Out;
+				//变量转换
+				LeftPWM=AvePWM+DifPWM/2;
+				RightPWM=AvePWM-DifPWM/2;
+				//限幅
+				if(LeftPWM>100){LeftPWM=100;}else if(LeftPWM<-100){LeftPWM=-100;}
+				if(RightPWM>100){RightPWM=100;}else if(RightPWM<-100){RightPWM=-100;}
+				//电机输出
+				Motor_SetPWM(1,LeftPWM);
+				Motor_SetPWM(2,RightPWM);
+				
+				
+			}
+			else{
+				Motor_SetPWM(1,0);
+				Motor_SetPWM(2,0);
+			}
+			
+		}
+		
 		
 		if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
 		{
