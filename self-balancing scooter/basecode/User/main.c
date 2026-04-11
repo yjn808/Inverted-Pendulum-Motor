1c8bfb249f1688fbbfc11a1f9a1b55421998d220
yjn808 <yjnysa@163.com>
2026-04-11 14:44:45 +0800

    PID_SPEED_Turn

--- self-balancing scooter/basecode/User/main.c
+++ self-balancing scooter/basecode/User/main.c
@@ -30,6 +30,9 @@ uint8_t KeyNum,RunFlag;
 int16_t LeftPWM,RightPWM;
 int16_t AvePWM,DifPWM;
 
+float LeftSpeed,RightSpeed;
+float AveSpeed,DifSpeed;
+
 PID_t AnglePID={
 	.Kp = 3,
 	.Ki = 0.1,
@@ -37,6 +40,24 @@ PID_t AnglePID={
 	
 	.OutMax=100,
 	.OutMin=-100
+};
+
+PID_t SpeedPID={
+	.Kp = 2,
+	.Ki = 0.05,
+	.Kd = 0,
+	
+	.OutMax=20,
+	.OutMin=-20
+};
+
+PID_t TurnPID={
+	.Kp = 0,
+	.Ki = 0,
+	.Kd = 0,
+	
+	.OutMax=50,
+	.OutMin=-50
 };
 
 
@@ -69,6 +90,8 @@ int main(void)
 			if(RunFlag==0)
 			{
 				PID_Init(&AnglePID);
+				PID_Init(&SpeedPID);
+				PID_Init(&TurnPID);
 				RunFlag=1;
 			}
 			else{
@@ -85,6 +108,22 @@ int main(void)
 		OLED_Printf(0,32,OLED_6X8,"T:%+05.2f",AnglePID.Target);
 		OLED_Printf(0,40,OLED_6X8,"A:%+05.1f",AnglePID);
 		OLED_Printf(0,48,OLED_6X8,"O:%+05.0f",AnglePID.Out);
+		
+		OLED_Printf(50,0,OLED_6X8,"Speed");
+		OLED_Printf(50,8,OLED_6X8,"%05.2f",SpeedPID.Kp);
+		OLED_Printf(50,16,OLED_6X8,"%05.2f",SpeedPID.Ki);
+		OLED_Printf(50,24,OLED_6X8,"%05.2f",SpeedPID.Kd);
+		OLED_Printf(50,32,OLED_6X8,"%+05.2f",SpeedPID.Target);
+		OLED_Printf(50,40,OLED_6X8,"%+05.1f",AveSpeed);
+		OLED_Printf(50,48,OLED_6X8,"%+05.0f",SpeedPID.Out);
+		
+		OLED_Printf(88,0,OLED_6X8,"Turn");
+		OLED_Printf(88,8,OLED_6X8,"%05.2f",TurnPID.Kp);
+		OLED_Printf(88,16,OLED_6X8,"%05.2f",TurnPID.Ki);
+		OLED_Printf(88,24,OLED_6X8,"%05.2f",TurnPID.Kd);
+		OLED_Printf(88,32,OLED_6X8,"%+05.2f",TurnPID.Target);
+		OLED_Printf(88,40,OLED_6X8,"%+05.1f",DifSpeed);
+		OLED_Printf(88,48,OLED_6X8,"%+05.0f",TurnPID.Out);
 		OLED_Update();
 		
 		
@@ -113,6 +152,32 @@ int main(void)
 				{
 					AnglePID.Kd=atof(Value);
 				}
+				//Encoder
+				else if(strcmp(Name,"SpeedKp") == 0)
+				{
+					SpeedPID.Kp=atof(Value);
+				}
+				else if(strcmp(Name,"SpeedKi") == 0)
+				{
+					SpeedPID.Ki=atof(Value);
+				}
+				else if(strcmp(Name,"SpeedKd") == 0)
+				{
+					SpeedPID.Kd=atof(Value);
+				}
+				//Turn
+				else if(strcmp(Name,"TurnKp") == 0)
+				{
+					TurnPID.Kp=atof(Value);
+				}
+				else if(strcmp(Name,"TurnKi") == 0)
+				{
+					TurnPID.Ki=atof(Value);
+				}
+				else if(strcmp(Name,"TurnKd") == 0)
+				{
+					TurnPID.Kd=atof(Value);
+				}
 			}
 			else if (strcmp(Tag, "joystick") == 0)
 			{
@@ -121,8 +186,8 @@ int main(void)
 				int8_t RH = atoi(strtok(NULL, ","));
 				int8_t RV = atoi(strtok(NULL, ","));
 				
-				AnglePID.Target=LV/10;
-				DifPWM=RH/2;
+				SpeedPID.Target=LV/25.0;
+				SpeedPID.Target=RH/25.0;
 				
 				
 			}
@@ -131,13 +196,13 @@ int main(void)
 		}
 		
 		
-		BlueSerial_Printf("[plot,%f,%f,%f]",AnglePID.Target,Angle);
+		BlueSerial_Printf("[plot,%f,%f]",TurnPID.Target,DifSpeed);
 		
 	}
 }
 void TIM1_UP_IRQHandler(void)
 {
-	static uint16_t Count0;
+	static uint16_t Count0,Count1;
 	
 	
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
@@ -152,8 +217,12 @@ void TIM1_UP_IRQHandler(void)
 		{
 			Count0=0;
 			MPU6050_GetData(&AX,&AY,&AZ,&GX,&GY,&GZ);
+			
+			GY+=10;
 		
 			AngleAcc=-atan2(AX,AZ)/3.14159*180;
+			
+			AngleAcc-=0.4;
 			
 			AngleGyro=Angle+GY/32768.0*2000*0.01;
 			
@@ -188,6 +257,34 @@ void TIM1_UP_IRQHandler(void)
 				Motor_SetPWM(2,0);
 			}
 			
+		}
+		
+		//读取编码器速度和差分
+		Count1++;
+		//作为外环调控周期大于内环
+		if(Count1>=50)
+		{
+			Count1=0;
+			LeftSpeed=Encoder_Get(1)/44.0/0.05/9.27666;
+			RightSpeed=Encoder_Get(2)/44.0/0.05/9.27666;
+			
+			//变量转换
+			AveSpeed=(LeftSpeed+RightSpeed)/2.0;
+			DifSpeed=LeftSpeed-RightSpeed;
+			
+			if(RunFlag)
+			{
+				//速度环
+				SpeedPID.Actual=AveSpeed;
+				PID_Update(&SpeedPID);
+				AnglePID.Target=SpeedPID.Out;
+				
+				//转向环
+				TurnPID.Actual=DifPWM;
+				PID_Update(&TurnPID);
+				DifPWM=TurnPID.Out;
+				
+			}
 		}
 		
 		
